9217c09c3a6837b93800dd1e70157beaf56f4ae2
徐志腾 <850592150@qq.com>
2020-08-05 02:23:42 +0800

    提交我的项目

---
+++ src/com/xzt/test/My.java
@@ -0,0 +1,11 @@
+package com.xzt.test;
+
+import java.util.Date;
+
+public class My {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.getTime());
+    }
+}
